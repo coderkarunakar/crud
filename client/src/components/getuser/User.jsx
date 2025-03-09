db4f84fc54e106e762a970cfa6a1a5c7fd781d93
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-09 21:36:48 +0530

    created a table with hard coded data

--- client/src/components/getuser/User.jsx
+++ client/src/components/getuser/User.jsx
@@ -6,6 +6,27 @@ const User = () => {
     return(
         <div className='userTable'>
             <Link to = {"/add"}>Add User</Link>
+            <table border={1} cellPadding={10} cellSpacing={0}>
+                <thead>
+                    <tr>
+                        <th>Sl.No</th>
+                        <th>user Name</th>
+                        <th>User Email</th>
+                        <th>Actions</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>Karunakar</td>
+                        <td>Karunakar@gmail.com</td>
+                        <td><button>Delete</button></td>
+                        {/* adding a link to move to edit page */}
+                        <Link to={'/edit'}>Edit</Link>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
