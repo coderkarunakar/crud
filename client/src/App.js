7a6b450e86056327508ce5e55971c2effe8afe5b
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-09 21:23:01 +0530

    installed all packages , google fonts, and added routes to this application

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path:"/",
+      element: "Home page",
+    },
+    {
+      path:"/add",
+      element: "User add page",
+    },
+    {
+      path:"/edit",
+      element: "Update users page",
+    }
+
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* this provides routes to this complete application */}
+    <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
